2c9c164896e700784d8dbd958ea1bf8f08f54df0
admin <swacc0101@gmail.com>
2025-03-19 09:11:55 +0900

    init

---
+++ src/pipe/positive-int.pipe.ts
@@ -0,0 +1,9 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class PositiveIntPipe implements PipeTransform {
+  transform(value:number, metadata: ArgumentMetadata): any {
+    return undefined;
+  }
+
+}
